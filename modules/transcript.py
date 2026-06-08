424b1d1d23745090bca6bcb420afcdb262aed8ee
kevinTB <kt6037792@gmail.com>
2026-06-08 18:05:27 +0000

    Fix transcript API

--- modules/transcript.py
+++ modules/transcript.py
@@ -1,20 +1,12 @@
 from youtube_transcript_api import YouTubeTranscriptApi
 
+
 def get_transcript(video_id):
+    transcript = YouTubeTranscriptApi.get_transcript(
+        video_id,
+        languages=["es", "en"]
+    )
 
-    transcript_list = YouTubeTranscriptApi.list_transcripts(video_id)
-
-    for transcript in transcript_list:
-        print(
-            transcript.language,
-            transcript.language_code,
-            transcript.is_generated
-        )
-
-    transcript = transcript_list.find_transcript(["es", "en"])
-
-    data = transcript.fetch()
-
-    text = " ".join([x["text"] for x in data])
+    text = " ".join(item["text"] for item in transcript)
 
     return text
